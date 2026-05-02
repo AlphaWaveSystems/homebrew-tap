d50dfcc134905c621a6affd6937a8cc805b84129
Patrick Bertsch <patrick@alphawavesystems.com>
2026-05-02 23:31:04 +0000

    chore: update probe to v0.7.0

--- Formula/probe.rb
+++ Formula/probe.rb
@@ -1,24 +1,24 @@
 class Probe < Formula
   desc "E2E testing CLI for Flutter apps using ProbeScript"
   homepage "https://flutterprobe.dev"
-  version "0.6.0"
+  version "0.7.0"
   license "BUSL-1.1"
 
   on_macos do
     on_arm do
       url "https://github.com/AlphaWaveSystems/flutter-probe/releases/download/v#{version}/probe-darwin-arm64"
-      sha256 "a4cc2c70f0623552e2bd3622d6d4b4ab9e31ce72345b8c67063d79cfa1fd4547"
+      sha256 "d413a0718b42f8a4ae7ca56f2587a204ad773ffcc614dc21424747c1fc2290c6"
     end
     on_intel do
       url "https://github.com/AlphaWaveSystems/flutter-probe/releases/download/v#{version}/probe-darwin-amd64"
-      sha256 "e376f2681e2cce22d7f13f77f7e208826286614efb6b0a2b4373c71f90223ec8"
+      sha256 "be78886d0040e6ac68809a21944aa19419489c90da36c920d6b46b4ca32c6328"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/AlphaWaveSystems/flutter-probe/releases/download/v#{version}/probe-linux-amd64"
-      sha256 "cad185a6fee375161bba6b0d61c2cfe5c7159f631c6c856a8034b8fce690dcfd"
+      sha256 "1d149fd01eb935dad1237f1d279de5dc0aa343d4518e784a57bf5d561194fb44"
     end
   end
 
